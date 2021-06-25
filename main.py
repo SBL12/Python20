ba2e47c86bb2ff793b12b15c37f22f28a5d5739e
Suyash L <81131181+SBL12@users.noreply.github.com>
2021-06-25 01:42:10 +0000

    Something

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ answer = input ("Hello " + name + "! Ready to play? YES or No ")
 print()
 if answer.upper() == "YES":
   question1 = input ("What color would you chose? (Chose A, or B)\na) Blue \nB) Red\n") 
-  question2 = input ("What game would you play (ChooseA, or B)\nA) Minecraft \nB Fortnite\n")
+  question2 = input ("What game would you play (Choose A, or B)\nA) Minecraft \nB Fortnite\n")
   question3 = input ("What sport would you prefer? (Choose A, or B)\nA) Tennis \nB) Basketball\n")
   choise = question1 + question2 + question3
   wait = input ("Do you want to know your favorite marvel hero? YES or No: ")
